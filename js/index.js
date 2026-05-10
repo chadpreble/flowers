e4643a1d7ef8bed54c001f56892190fa6cd2bd7c
Chad Preble <preblecm@gmail.com>
2026-05-10 09:18:02 -0500

    Mother's Day

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Happy Birthday, Babe!`.split('')
+const text = `Mother's Day...`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I Love You!').split('')
+    const titles = ('I Love You So Much! - Chad').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
